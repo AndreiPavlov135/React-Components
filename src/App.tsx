1ef43996d847f38219cbdce685d2d35c6d0022b4
AndreiPavlov135 <9000andrey@gmail.com>
2023-03-15 23:53:08 +0300

    feat: add routing between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Main from './components/Main/Main';
+import AboutUs from './components/AboutUs/AboutUs';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import Header from './components/Header/Header';
 
 function App() {
-  return <div>test</div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
