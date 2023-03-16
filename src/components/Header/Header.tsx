20475a31bdf54a3ebb0147e182eadee54940854d
AndreiPavlov135 <9000andrey@gmail.com>
2023-03-17 01:45:57 +0300

    feat: add input to main and styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 export default class Header extends React.Component {
   render() {
     return (
-      <header>
-        <Link to="/">Main</Link>
-        <Link to="/aboutus">About Us</Link>
+      <header className="header">
+        <Link className="header__link main" to="/">
+          Main
+        </Link>
+        <Link className="header__link about-us" to="/aboutus">
+          About Us
+        </Link>
       </header>
     );
   }
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import './Main.css';
 
 export default class Main extends React.Component {
   render() {
-    return <h1>Main</h1>;
+    return (
+      <div className="text-field">
+        <label className="text-field__label" htmlFor="search">
+          Search
+        </label>
+        <div className="text-field__group">
+          <input className="text-field__input" type="search" id="search" name="search" />
+          <button className="text-field__btn" type="button">
+            Search
+          </button>
+        </div>
+      </div>
+    );
   }
 }
