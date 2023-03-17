18374942323ae38e6991a963d281d1c35a4e33a7
AndreiPavlov135 <9000andrey@gmail.com>
2023-03-18 02:22:36 +0300

    refactor: add new component serchbar

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import './Main.css';
+import SearchBar from '../Utils/SearchBar';
 
 export default class Main extends React.Component {
   render() {
     return (
-      <div className="text-field">
-        <label className="text-field__label" htmlFor="search">
-          Search
-        </label>
-        <div className="text-field__group">
-          <input className="text-field__input" type="search" id="search" name="search" />
-          <button className="text-field__btn" type="button">
-            Search
-          </button>
-        </div>
-      </div>
+      <>
+        <SearchBar />
+      </>
     );
   }
 }
---
+++ src/components/Utils/SearchBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Utils-styles/Search-bar.css';
+
+export default class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="text-field">
+        <label className="text-field__label" htmlFor="search">
+          Search
+        </label>
+        <div className="text-field__group">
+          <input className="text-field__input" type="search" id="search" name="search" />
+          <button className="text-field__btn" type="button">
+            Search
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
