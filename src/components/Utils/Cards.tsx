d03a3246f7370cb2ea5c9cbf7008f76c94976f04
AndreiPavlov135 <9000andrey@gmail.com>
2023-03-19 18:46:18 +0300

    feat: add input value

---
+++ src/components/Utils/Cards.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import './Utils-styles/Cards.css';
+
+export default class Cards extends React.Component {}
--- src/components/Utils/SearchBar.tsx
+++ src/components/Utils/SearchBar.tsx
@@ -2,18 +2,36 @@ import React from 'react';
 import './Utils-styles/Search-bar.css';
 
 export default class SearchBar extends React.Component {
+  inputValue = '';
+
+  getInputValue(e: React.ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    this.inputValue = e.target.value;
+  }
+
+  componentWillUnmount() {
+    localStorage.value = this.inputValue;
+  }
+
   render() {
     return (
-      <div className="text-field">
-        <label className="text-field__label" htmlFor="search">
+      <div className="search-field">
+        <label className="search-field__label" htmlFor="search">
           Search
         </label>
-        <div className="text-field__group">
-          <input className="text-field__input" type="search" id="search" name="search" />
-          <button className="text-field__btn" type="button">
+        <form className="search-field__group">
+          <input
+            className="search-field__input"
+            type="search"
+            id="search"
+            name="search"
+            onChange={(e) => this.getInputValue(e)}
+          />
+          <button className="search-field__btn" type="button">
             Search
           </button>
-        </div>
+        </form>
+        <span className="search-field__result">Input value: {localStorage.value}</span>
       </div>
     );
   }
