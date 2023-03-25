de45a7df48069909f8ddc746bc70658a86d71eca
AndreiPavlov135 <9000andrey@gmail.com>
2023-03-25 20:30:28 +0300

    feat: add forms page

---
+++ src/components/Forms/Forms.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+export default class Forms extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    );
+  }
+}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,20 +3,29 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 export default class Header extends React.Component {
+  urlData = document.location.href;
+  url = this.urlData.split('/').slice(-1).toString();
+  state = { page: this.url || 'main' };
+  changePage = (page: string) => {
+    this.setState({ page });
+  };
   render() {
-    const urlData = document.location.href;
-    const url = urlData.split('/').slice(-1).toString();
-    const page = url ? 'ABOUT US' : 'MAIN';
-
     return (
       <header className="header">
         <Link className="header__link main" to="/">
           Main
         </Link>
-        <Link className="header__link about-us" to="/aboutus">
+        <Link
+          className="header__link about-us"
+          to="/about-us"
+          onClick={() => this.changePage(this.url)}
+        >
           About Us
         </Link>
-        <span className="page-name">Page: {page}</span>
+        <Link className="header__link" to="/forms" onClick={() => this.changePage(this.url)}>
+          Forms
+        </Link>
+        <span className="page-name">Page: {this.state.page}</span>
       </header>
     );
   }
